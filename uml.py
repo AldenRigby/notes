56b78315236dc519e7bf0c3a725054e446a4f81b
AldenRigby <alden.rigby@ucas-edu.net>
2024-10-14 11:28:30 -0600

    grahhhhhh

--- uml.py
+++ uml.py
@@ -1,5 +1,24 @@
-#uml modeling thingy
+#uml modeling thingy: UNIFIED MODELING LANGUAGE. YIPPEE
 #standartized notation to use by object oriented thing
 #structural diagrams and behavioral diagrams
 #structural: composite structure, deployment, package, profile, class, object, component
 #behavioral: activity, use case, state machine, interaction {sequence, communicate, interaction overview, timing}
+#class: central. relationships between. association, inheritance, aggregation
+#component: dependencies and stuff
+#deployment: physical aspect of thingies
+#object: shows instance
+#package: shows packages and dependencies
+#composite structure: like class but components and sutff
+
+#profile: sterotypes
+#activty choice thing
+#state machine: describe system behavior
+#communication communication
+#uagwefukyawgefhawo8yliuf
+
+#class diagrams impotrant
+#______________
+#| class      | need to know V type (name: string)
+#| attributes | +- attribute1:type( = default) (+ public, - private, # protected (no change))
+#| modules    | +- operation(params): returnType
+#______________
